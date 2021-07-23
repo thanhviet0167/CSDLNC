2279fb8ac8765eecfa0865a864fda58f968552d2
VIỆT ĐINH THÀNH <18126037@student.hcmus.edu.vn>
2021-07-23 21:44:19 +0700

    Add page admin app

--- admin_app/src/App.js
+++ admin_app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './Layout/Pages/Home/Home';
+import Login from './Layout/Pages/Login/Login';
+import Register from './Layout/Pages/Login/Register';
+import Forgot from './Layout/Pages/Login/Forgot';
+import Cards from './Layout/Pages/Card/Cards';
+import Chart from './Layout/Pages/Chart/Chart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+          <Switch>
+              <Route path="/" exact><Home />  </Route>
+              <Route path="/login" exact><Login/></Route>
+              <Route path="/sign-up" exact><Register/></Route>
+              <Route path="/forgot" exact><Forgot/></Route>
+              <Route path="/cards" exact><Cards/></Route>
+              <Route path="/chart" exact><Chart/></Route>
+          </Switch>
+        </Router>
+  
+      </div>
   );
 }
 
