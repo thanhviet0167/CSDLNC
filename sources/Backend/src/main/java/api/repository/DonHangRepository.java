91a1e2e05b49ccac23589ed7a490f1ee06a41446
ddlinh <18126023@student.hcmus.edu.vn>
2021-08-18 04:48:45 +0700

    Update API 20,21

--- sources/Backend/src/main/java/api/repository/DonHangRepository.java
+++ sources/Backend/src/main/java/api/repository/DonHangRepository.java
@@ -2,8 +2,16 @@ package api.repository;
 
 import api.domain.DonHang;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DonHangRepository extends JpaRepository<DonHang, Long> {
+
+    @Query(value = "SELECT DonHang.* FROM DonHang JOIN GioHang ON DonHang.GioHang = GioHang.MaGioHang WHERE GioHang.KhachHang LIKE :customer AND (:status is null or DonHang.TrangThaiThanhToan = :status)"
+            , nativeQuery = true)
+    List<DonHang> findAllByKhachHang(@Param("customer") String khachHang, @Param("status") Integer status);
 }
